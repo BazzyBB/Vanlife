314d96c9fff00835274733dd11d865733e07c4c4
BazzyBB <106365563+BazzyBB@users.noreply.github.com>
2024-07-30 21:58:31 -0500

    Update Reviews.jsx

--- pages/Host/Reviews.jsx
+++ pages/Host/Reviews.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BsStarFill } from "react-icons/bs"
-import ReviewsGraph from "../assets/images/reviews-graph.png"
+import ReviewsGraph from "../pages/assets/images/reviews-graph.png"
 
 export default function Reviews() {
     const reviewsData = [
