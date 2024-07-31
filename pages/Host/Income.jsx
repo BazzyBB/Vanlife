11e8869ee28de6177e69fdc41dc96b3314a19a8e
BazzyBB <106365563+BazzyBB@users.noreply.github.com>
2024-07-30 22:08:06 -0500

    Update Income.jsx

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import incomeGraph from "../Host/assets/images/income-graph.png"
+import incomeGraph from "../../assets/images/income-graph.png"
 
 export default function Income() {
     const transactionsData = [
